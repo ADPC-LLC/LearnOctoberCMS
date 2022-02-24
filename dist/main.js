164daf7be6ab0d31e5837032fb57add6049d69b3
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2022-02-24 19:59:25 +0400

    Updated Theme directories

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello World! This came from Webpack One Configuration");
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const stylesHandler = "style-loader";
 const config = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./themes/olympos/assets/dist"),
   },
   devServer: {
     open: true,
