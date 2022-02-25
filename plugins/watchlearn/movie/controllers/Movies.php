4fb9aca3bae96b924c10693b2a6f71175f59edeb
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2022-02-25 16:59:26 +0400

    Removed old pages

--- plugins/watchlearn/movie/controllers/Movies.php
+++ plugins/watchlearn/movie/controllers/Movies.php
@@ -6,18 +6,18 @@ use BackendMenu;
 class Movies extends Controller
 {
     public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
-    
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
 
     public $requiredPermissions = [
-        'watchlearn.movie.manage_movie' 
+        'watchlearn.movies.manage_movie'
     ];
 
     public function __construct()
     {
         parent::__construct();
-        BackendMenu::setContext('Watchlearn.Movie', 'Movie Library', 'Movies');
+        BackendMenu::setContext('watchlearn.movies', 'Movie Library', 'Movies');
     }
 }
